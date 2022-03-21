ce571228e716e6cff820add112a7a162c893bc66
Maycon Morais <97128625+maycomorais@users.noreply.github.com>
2022-03-21 15:54:06 -0300

    Add files via upload

---
+++ JOKENPO.js
@@ -0,0 +1,41 @@
+console.clear();
+const prompt = require('prompt-sync')();
+let jogo = ['pedra', 'papel', 'tesoura'];
+let player = ''
+let computador = '';
+let escolha = false;
+let rodadas = +prompt('Digite a quantidade de rodadas: ')
+let contV = 0
+let contE = 0
+let contD = 0
+
+console.log('JOKENPO');
+
+for (i = 0; i < rodadas; i++) {
+    while (escolha == false){
+        player = prompt('Escolha "Pedra", "Papel" ou "Tesoura"]": ').toLowerCase();
+        if(player == 'pedra' || player == 'papel' || player == 'tesoura'){
+            escolha = true;
+        }
+    console.log();
+    }
+    computador = jogo[Math.floor(Math.random() * jogo.length)];
+    console.log(computador);
+    console.log();
+    if((player == 'pedra' && computador == 'tesoura') || (player == 'papel' && computador == 'pedra') || (player == 'tesoura' && computador == 'papel')) {
+        contV++;
+    } else if (player == computador) {
+        contE++;
+    } else {
+        contD++;
+    } 
+    escolha = false
+}
+console.log();
+if(contV > contD) {
+    console.log(`V = ${contV} E = ${contE} D = ${contD} | Você venceu!`);
+} else if (contV == contD) {
+    console.log(`V = ${contV} E = ${contE} D = ${contD} | Você empatou!`);
+} else {
+    console.log(`V = ${contV} | E = ${contE} | D = ${contD} | Você perdeu!`);
+}
